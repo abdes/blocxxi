acf9bf7c6093f5b454ff402e135d370970f4ad98
Abdessattar Sassi <abde.sassi@gmail.com>
2018-06-20 11:52:15 +0400

    regenrate glad, resolve compiler warnings, resturcture compiler flags

--- ndagent/src/main.cpp
+++ ndagent/src/main.cpp
@@ -17,19 +17,18 @@
 #include <p2p/kademlia/parameters.h>
 #include <p2p/kademlia/session.h>
 
-
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 #include <stdio.h>
-#include <glad/glad.h>    // This example is using gl3w to access OpenGL functions. You may freely use any other OpenGL loader such as: glew, glad, glLoadGen, etc.
+// This example is using gl3w to access OpenGL functions. You may freely use any
+// other OpenGL loader such as: glew, glad, glLoadGen, etc.
+#include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
-static void glfw_error_callback(int error, const char* description)
-{
+static void glfw_error_callback(int error, const char *description) {
   fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
-
 
 namespace bpo = boost::program_options;
 namespace kad = blocxxi::p2p::kademlia;
@@ -86,20 +85,20 @@ class ConsoleRunner : public RunnerBase {
   boost::asio::signal_set signals_;
 };
 
-using blocxxi::p2p::kademlia::Node;
-using blocxxi::p2p::kademlia::RoutingTable;
-using blocxxi::p2p::kademlia::ResponseDispatcher;
-using blocxxi::p2p::kademlia::Network;
-using blocxxi::p2p::kademlia::Channel;
-using blocxxi::p2p::kademlia::AsyncUdpChannel;
-using blocxxi::p2p::kademlia::MessageSerializer;
-using blocxxi::p2p::kademlia::Engine;
-using blocxxi::p2p::kademlia::Session;
-using blocxxi::p2p::kademlia::KeyType;
 using blocxxi::nat::PortMapper;
+using blocxxi::p2p::kademlia::AsyncUdpChannel;
+using blocxxi::p2p::kademlia::Channel;
+using blocxxi::p2p::kademlia::Engine;
+using blocxxi::p2p::kademlia::KeyType;
+using blocxxi::p2p::kademlia::MessageSerializer;
+using blocxxi::p2p::kademlia::Network;
+using blocxxi::p2p::kademlia::Node;
+using blocxxi::p2p::kademlia::ResponseDispatcher;
+using blocxxi::p2p::kademlia::RoutingTable;
+using blocxxi::p2p::kademlia::Session;
 
 using NetworkType =
-blocxxi::p2p::kademlia::Network<AsyncUdpChannel, MessageSerializer>;
+    blocxxi::p2p::kademlia::Network<AsyncUdpChannel, MessageSerializer>;
 using EngineType = blocxxi::p2p::kademlia::Engine<RoutingTable, NetworkType>;
 
 int main(int argc, char **argv) {
@@ -113,17 +112,16 @@ int main(int argc, char **argv) {
   try {
     // Command line arguments
     bpo::options_description desc("Allowed options");
-    desc.add_options()
-        ("help", "produce hel message")
-        ("nat,n", bpo::value<std::string>(&nat_spec)->default_value(""),
-         "NAT specification as one of the following accepted formats: "
-         "'upnp','extip:1.2.3.4[:192.168.1.5]'. If not present, best effort "
-         "selection of the address will be automatically done.")
-        ("ipv6", bpo::value<std::string>(&ipv6_address)->default_value("::1"),
-         "specify an IPV6 address to bind to as well.")
-        ("port,p", bpo::value<unsigned short>(&port)->default_value(9000),
-         "port number")
-        ("bootstrap,b", bpo::value<std::vector<std::string>>(&boot_list));
+    desc.add_options()("help", "produce hel message")(
+        "nat,n", bpo::value<std::string>(&nat_spec)->default_value(""),
+        "NAT specification as one of the following accepted formats: "
+        "'upnp','extip:1.2.3.4[:192.168.1.5]'. If not present, best effort "
+        "selection of the address will be automatically done.")(
+        "ipv6", bpo::value<std::string>(&ipv6_address)->default_value("::1"),
+        "specify an IPV6 address to bind to as well.")(
+        "port,p", bpo::value<unsigned short>(&port)->default_value(9000),
+        "port number")("bootstrap,b",
+                       bpo::value<std::vector<std::string>>(&boot_list));
 
     bpo::variables_map bpo_vm;
     bpo::store(bpo::parse_command_line(argc, argv, desc), bpo_vm);
@@ -152,8 +150,7 @@ int main(int argc, char **argv) {
     auto mapper = blocxxi::nat::GetPortMapper(nat_spec);
     if (!mapper) {
       BXLOG_TO_LOGGER(logger, error, "Failed to initialize the network.");
-      BXLOG_TO_LOGGER(logger,
-                      error,
+      BXLOG_TO_LOGGER(logger, error,
                       "Try to explicitly set the local and external addresses "
                       "using the 'extip:' NAT spec.");
       return -1;
@@ -166,11 +163,11 @@ int main(int argc, char **argv) {
     auto my_node = Node{Node::IdType::RandomHash(), mapper->ExternalIP(), port};
     auto rt = RoutingTable{my_node, blocxxi::p2p::kademlia::CONCURRENCY_K};
 
-    auto ipv4 = AsyncUdpChannel::ipv4(io_context,
-                                      mapper->InternalIP(),
+    auto ipv4 = AsyncUdpChannel::ipv4(io_context, mapper->InternalIP(),
                                       std::to_string(port));
 
-    auto ipv6 = AsyncUdpChannel::ipv6(io_context, ipv6_address, std::to_string(port));
+    auto ipv6 =
+        AsyncUdpChannel::ipv6(io_context, ipv6_address, std::to_string(port));
 
     auto serializer = std::make_unique<MessageSerializer>(my_node.Id());
     auto network = NetworkType{io_context, std::move(serializer),
@@ -185,10 +182,7 @@ int main(int argc, char **argv) {
     auto session = SessionType(io_context, std::move(engine));
     session.Start();
 
-    boost::thread server_thread([&io_context]() {
-        io_context.run();
-	});
-
+    boost::thread server_thread([&io_context]() { io_context.run(); });
 
     if (!boot_list.empty()) {
       //
@@ -196,25 +190,27 @@ int main(int argc, char **argv) {
       //
       auto value = SessionType::DataType{0x01, 0x02};
       auto key = KeyType::RandomHash();
-      session.StoreValue(key, value, [&logger, &session, &value,
-          &key](std::error_code const &failure) {
-        if (failure) {
-          BXLOG_TO_LOGGER(logger, error, "store value failed");
-        }
-        //
-        // FIND the value
-        //
-        value.clear();
-        session.FindValue(key, [&logger](std::error_code const &failure,
-                                         SessionType::DataType const &value) {
-          if (failure) {
-            BXLOG_TO_LOGGER(logger, error, "find value failed");
-          } else {
-            BXLOG_TO_LOGGER(logger, info, "value: {} {}", value[0], value[1]);
-          }
-        });
-
-      });
+      session.StoreValue(
+          key, value,
+          [&logger, &session, &value, &key](std::error_code const &failure) {
+            if (failure) {
+              BXLOG_TO_LOGGER(logger, error, "store value failed");
+            }
+            //
+            // FIND the value
+            //
+            value.clear();
+            session.FindValue(
+                key, [&logger](std::error_code const &failure,
+                               SessionType::DataType const &value) {
+                  if (failure) {
+                    BXLOG_TO_LOGGER(logger, error, "find value failed");
+                  } else {
+                    BXLOG_TO_LOGGER(logger, info, "value: {} {}", value[0],
+                                    value[1]);
+                  }
+                });
+          });
     }
 
 #ifdef NDAGENT_CONSOLE
@@ -227,72 +223,86 @@ int main(int argc, char **argv) {
       server_thread.join();
     });
     runner.AwaitStop();
-#else // NDAGENT_CONSOLE
+#else  // NDAGENT_CONSOLE
     //
     // Use GUI
     //
     // Setup window
     glfwSetErrorCallback(glfw_error_callback);
-    if (!glfwInit())
-      return 1;
+    if (!glfwInit()) return 1;
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #if __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "ImGui GLFW+OpenGL3 example", NULL, NULL);
-    if (!window)
-    {
+    GLFWwindow *window =
+        glfwCreateWindow(1280, 720, "ImGui GLFW+OpenGL3 example", NULL, NULL);
+    if (!window) {
       glfwTerminate();
       exit(EXIT_FAILURE);
     }
 
     glfwMakeContextCurrent(window);
-    gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
-    glfwSwapInterval(1); // Enable vsync
+    gladLoadGL((GLADloadfunc)glfwGetProcAddress);
+    glfwSwapInterval(1);  // Enable vsync
 
     // Setup Dear ImGui binding
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable Gamepad Controls
+    ImGuiIO &io = ImGui::GetIO();
+    (void)io;
+    // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard
+    // Controls io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable
+    // Gamepad Controls
 
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init();
 
     // Setup style
     ImGui::StyleColorsDark();
-    //ImGui::StyleColorsClassic();
+    // ImGui::StyleColorsClassic();
 
     // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+    // - If no fonts are loaded, dear imgui will use the default font. You can
+    // also load multiple fonts and use ImGui::PushFont()/PopFont() to select
+    // them.
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you
+    // need to select the font among multiple.
+    // - If the file cannot be loaded, the function will return NULL. Please
+    // handle those errors in your application (e.g. use an assertion, or
+    // display an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and
+    // stored into a texture when calling
+    // ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame
+    // below will call.
     // - Read 'misc/fonts/README.txt' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != NULL);
+    // - Remember that in C/C++ if you want to include a backslash \ in a string
+    // literal you need to write a double backslash \\ !
+    // io.Fonts->AddFontDefault();
+    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
+    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
+    // ImFont* font =
+    // io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f,
+    // NULL, io.Fonts->GetGlyphRangesJapanese()); IM_ASSERT(font != NULL);
 
     bool show_demo_window = true;
     bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     // Main loop
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)) {
       // Poll and handle events (inputs, window resize, etc.)
-      // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-      // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
-      // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-      // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+      // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to
+      // tell if dear imgui wants to use your inputs.
+      // - When io.WantCaptureMouse is true, do not dispatch mouse input data to
+      // your main application.
+      // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input
+      // data to your main application. Generally you may always pass all inputs
+      // to dear imgui, and hide them from your application based on those two
+      // flags.
       glfwPollEvents();
 
       // Start the ImGui frame
@@ -301,39 +311,56 @@ int main(int argc, char **argv) {
       ImGui::NewFrame();
 
       // 1. Show a simple window.
-      // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets automatically appears in a window called "Debug".
+      // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets
+      // automatically appears in a window called "Debug".
       {
         static float f = 0.0f;
         static int counter = 0;
-        ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
-        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-        ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+        ImGui::Text("Hello, world!");  // Display some text (you can use a
+                                       // format string too)
+        ImGui::SliderFloat(
+            "float", &f, 0.0f,
+            1.0f);  // Edit 1 float using a slider from 0.0f to 1.0f
+        ImGui::ColorEdit3(
+            "clear color",
+            (float *)&clear_color);  // Edit 3 floats representing a color
 
-        ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our windows open/close state
+        ImGui::Checkbox("Demo Window",
+                        &show_demo_window);  // Edit bools storing our windows
+                                             // open/close state
         ImGui::Checkbox("Another Window", &show_another_window);
 
-        if (ImGui::Button("Button"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
+        if (ImGui::Button(
+                "Button"))  // Buttons return true when clicked (NB: most
+                            // widgets return true when edited/activated)
           counter++;
         ImGui::SameLine();
         ImGui::Text("counter = %d", counter);
 
-        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
+                    1000.0f / ImGui::GetIO().Framerate,
+                    ImGui::GetIO().Framerate);
       }
 
-      // 2. Show another simple window. In most cases you will use an explicit Begin/End pair to name your windows.
-      if (show_another_window)
-      {
+      // 2. Show another simple window. In most cases you will use an explicit
+      // Begin/End pair to name your windows.
+      if (show_another_window) {
         ImGui::Begin("Another Window", &show_another_window);
         ImGui::Text("Hello from another window!");
-        if (ImGui::Button("Close Me"))
-          show_another_window = false;
+        if (ImGui::Button("Close Me")) show_another_window = false;
         ImGui::End();
       }
 
-      // 3. Show the ImGui demo window. Most of the sample code is in ImGui::ShowDemoWindow(). Read its code to learn more about Dear ImGui!
-      if (show_demo_window)
-      {
-        ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiCond_FirstUseEver); // Normally user code doesn't need/want to call this because positions are saved in .ini file anyway. Here we just want to make the demo initial state a bit more friendly!
+      // 3. Show the ImGui demo window. Most of the sample code is in
+      // ImGui::ShowDemoWindow(). Read its code to learn more about Dear ImGui!
+      if (show_demo_window) {
+        ImGui::SetNextWindowPos(
+            ImVec2(650, 20),
+            ImGuiCond_FirstUseEver);  // Normally user code doesn't need/want to
+                                      // call this because positions are saved
+                                      // in .ini file anyway. Here we just want
+                                      // to make the demo initial state a bit
+                                      // more friendly!
         ImGui::ShowDemoWindow(&show_demo_window);
       }
 
@@ -351,6 +378,10 @@ int main(int argc, char **argv) {
       glfwSwapBuffers(window);
     }
 
+    // Shutdown
+    io_context.stop();
+    server_thread.join();
+
     // Cleanup
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
@@ -359,7 +390,7 @@ int main(int argc, char **argv) {
     glfwDestroyWindow(window);
     glfwTerminate();
 
-#endif // NDAGENT_CONSOLE
+#endif  // NDAGENT_CONSOLE
 
   } catch (std::exception &e) {
     BXLOG_TO_LOGGER(logger, error, "Error: {}", e.what());
