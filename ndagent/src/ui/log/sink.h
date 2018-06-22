dc8ef4f4dcf7084a71cabd527a10e04882ed5ef8
Abdessattar Sassi <abde.sassi@gmail.com>
2018-06-22 23:06:30 +0400

    update theme and log ui

--- ndagent/src/ui/log/sink.h
+++ ndagent/src/ui/log/sink.h
@@ -11,7 +11,7 @@
 #include <spdlog/spdlog.h>
 
 #include <ui/style/theme.h>
-
+#include <imgui.h>
 
 namespace blocxxi {
 namespace debug {
@@ -19,51 +19,14 @@ namespace ui {
 
 class ImGuiLogSink : public spdlog::sinks::base_sink<std::mutex> {
  public:
-  void Clear() {
-    std::lock_guard<std::mutex> lock(_mutex);
-    records_.clear();
-  }
+  void Clear();
 
-  void Draw(const char* title, bool* p_open = NULL) {
-    ImGui::SetNextWindowSize(ImVec2(500, 400), ImGuiCond_FirstUseEver);
-    ImGui::Begin(title, p_open);
-    if (ImGui::Button("Clear")) Clear();
-    ImGui::SameLine();
-    display_filter_.Draw("Filter", -100.0f);
-    ImGui::Separator();
-    ImGui::BeginChild("scrolling", ImVec2(0, 0), false,
-                      ImGuiWindowFlags_HorizontalScrollbar);
-
-    Font font("Inconsolata");
-    font.Regular().MediumSize();
-    ImGui::PushFont(font.ImGuiFont());
-	for (auto record : records_) {
-      if (display_filter_.IsActive()) {
-            if (display_filter_.PassFilter(record.c_str(),
-                                           record.c_str() + record.size())) {
-          ImGui::TextWrapped(record.c_str());
-        }
-      } else {
-        ImGui::TextWrapped(record.c_str());
-      }
-    }
-    ImGui::PopFont();
-
-    if (scroll_to_bottom_) ImGui::SetScrollHere(1.0f);
-    scroll_to_bottom_ = false;
-    ImGui::EndChild();
-    ImGui::End();
-  }
+  void Draw(const char* title, bool* p_open = nullptr);
 
  protected:
-  void _sink_it(const spdlog::details::log_msg& msg) override {
-    records_.push_back(msg.formatted.str());
-    scroll_to_bottom_ = true;
-  }
+  void _sink_it(const spdlog::details::log_msg& msg) override;
 
-  void _flush() override {
-    // Your code here
-  }
+  void _flush() override;
 
  private:
   std::vector<std::string> records_;
