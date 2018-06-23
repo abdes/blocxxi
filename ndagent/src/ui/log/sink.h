a145423b126f0ad1725e902659a462649be88b17
Abdessattar Sassi <abde.sassi@gmail.com>
2018-06-23 17:10:50 +0400

    enhancing the log output window

--- ndagent/src/ui/log/sink.h
+++ ndagent/src/ui/log/sink.h
@@ -5,13 +5,14 @@
 
 #pragma once
 
-#include <vector>
+#include <vector>        // for the records vector
+#include <shared_mutex>  // for locking the records vector
 
 #include <spdlog/sinks/sink.h>
 #include <spdlog/spdlog.h>
 
-#include <ui/style/theme.h>
 #include <imgui.h>
+#include <ui/style/theme.h>
 
 namespace blocxxi {
 namespace debug {
@@ -21,6 +22,14 @@ class ImGuiLogSink : public spdlog::sinks::base_sink<std::mutex> {
  public:
   void Clear();
 
+  void ShowLogLevelsPopup();
+
+  void ShowLogFormatPopup();
+
+  void ToggleWrap() { wrap_ = !wrap_; }
+
+  void ToggleScrollLock() { scroll_lock_ = !scroll_lock_; }
+
   void Draw(const char* title, bool* p_open = nullptr);
 
  protected:
@@ -29,10 +38,32 @@ class ImGuiLogSink : public spdlog::sinks::base_sink<std::mutex> {
   void _flush() override;
 
  private:
-  std::vector<std::string> records_;
+  static const ImVec4 COLOR_WARN;
+  static const ImVec4 COLOR_ERROR;
+
+  struct LogRecord {
+    std::string text_;
+    std::size_t color_range_start_{0};
+    std::size_t color_range_end_{0};
+    const ImVec4& color_;
+    bool emphasis_{false};
+  };
+  std::vector<LogRecord> records_;
+  mutable std::shared_timed_mutex records_mutex_;
   ImGuiTextFilter display_filter_;
 
   bool scroll_to_bottom_;
+  bool wrap_{ false };
+  bool scroll_lock_{ false };
+
+  /// @name Log Format flags
+  //@{
+  bool show_time_{true};
+  bool show_thread_{true};
+  bool show_level_{true};
+  bool show_logger_{true};
+  bool show_source_{false};
+  //@}
 };
 
 }  // namespace ui
