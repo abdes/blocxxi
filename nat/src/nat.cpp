26962f2427a79d145da69af84f27bf1ced4615b2
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2022-08-23 14:31:22 +0400

    chore: update file headers to use standard template

--- nat/src/nat.cpp
+++ nat/src/nat.cpp
@@ -1,6 +1,8 @@
-//
-// Created by Abdessattar Sassi on 6/11/18.
-//
+//===----------------------------------------------------------------------===//
+// Distributed under the 3-Clause BSD License. See accompanying file LICENSE or
+// copy at https://opensource.org/licenses/BSD-3-Clause).
+// SPDX-License-Identifier: BSD-3-Clause
+//===----------------------------------------------------------------------===//
 
 #include <algorithm>
 #include <cctype>
