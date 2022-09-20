f9c7db3d29efda4c7b819186b7f5b317a9d9dd8e
abdes <abdes@users.noreply.github.com>
2022-09-20 13:04:00 +0000

    deploy: f8e7a7df3d0a4ebffdc2d15593552783ca872a12

--- blocxxi_master/html/_static/documentation_options.js
+++ blocxxi_master/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.0 (5af9e7118276)',
+    VERSION: '1.0.1 (f8e7a7df3d0a)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
